7a1c733f0a46057d844999d9e94bbfbeb36abb30
Yash Singh <yashsingh88088@gmail.com>
2023-12-21 15:30:32 +0530

    menu migration errors fixed

--- src/DynamicMenu.js
+++ src/DynamicMenu.js
@@ -3,18 +3,14 @@ import { Icon } from '@iconify/react';
 import './DynamicMenu.css';
 
 const DynamicMenu = () => {
-  const menuItems = useMemo(
-    () => ['HOME', 'ELECTRONICS', 'BOOKS', 'MUSIC', 'MOVIES', 'CLOTHING', 'GAMES'],
-    []
-  );
+  const menuItems = useMemo(() => ['HOME', 'ELECTRONICS', 'BOOKS', 'MUSIC', 'MOVIES', 'CLOTHING', 'GAMES'], []);
+  const moreOptions = useMemo(() => ['FURNITURE', 'ELECTRONICS', 'TRAVEL', 'BOTANICAL', 'CATEGORY NAME'], []);
+  const itemsToRender = useMemo(() => [...menuItems, ...moreOptions], [menuItems, moreOptions]);
 
-  const moreOptions = useMemo(
-    () => ['FURNITURE', 'ELECTRONICS', 'TRAVEL', 'BOTANICAL', 'CATEGORY NAME'],
-    []
-  );
 
   const [openMoreOptions, setOpenMoreOptions] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [windowWidth, setWindowWidth] = useState(window.screen.width); 
 
   const toggleMoreOptions = useCallback((item) => {
     setOpenMoreOptions((prevOpen) => !prevOpen);
@@ -33,45 +29,16 @@ const DynamicMenu = () => {
   );
 
   useEffect(() => {
-    const updateMenuForSmallScreens = () => {
-      const windowWidth = window.innerWidth;
-
-      if (windowWidth <= 768) {
-        // Clear the existing items in the dropdown list
-        const dropdownList = document.querySelector('.dropdown-list');
-        while (dropdownList.firstChild) {
-          dropdownList.removeChild(dropdownList.firstChild);
-        }
-
-        // Render menu items and more options in the dropdown list for small screens
-        const itemsToRender = [...menuItems, ...moreOptions];
-        itemsToRender.forEach((item, index) => {
-          const listItem = document.createElement('li');
-          listItem.innerText = item;
-          listItem.addEventListener('click', () => toggleMoreOptions(item));
-          listItem.classList.add('dropdown-list-item');
-          listItem.classList.toggle('selected', selectedItem === item);
-
-          if (selectedItem === item) {
-            const tickIcon = document.createElement('span');
-            tickIcon.innerText = '✔'; 
-            tickIcon.classList.add('tick-icon');
-            listItem.appendChild(tickIcon);
-          }
-
-          dropdownList.appendChild(listItem);
-        });
-      }
+    const handleResize = () => {
+      setWindowWidth(window.screen.width);
     };
 
-    updateMenuForSmallScreens();
-
-    window.addEventListener('resize', updateMenuForSmallScreens);
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', updateMenuForSmallScreens);
+      window.removeEventListener('resize', handleResize);
     };
-  }, [menuItems, moreOptions, selectedItem, toggleMoreOptions]);
+  }, []);
 
   useEffect(() => {
     document.addEventListener('click', closeDropdownOnOutsideClick);
@@ -80,6 +47,7 @@ const DynamicMenu = () => {
       document.removeEventListener('click', closeDropdownOnOutsideClick);
     };
   }, [closeDropdownOnOutsideClick, openMoreOptions]);
+
 
   return (
     <div className="dynamic-menu">
@@ -90,42 +58,58 @@ const DynamicMenu = () => {
             alt=""
             className="logo"
           />
-          <span className={`logo-text ${window.innerWidth <= 768 ? 'small' : ''}`}>E-COMM</span>
+          <span className="logo-text">E-COMM</span>
         </div>
 
-        <div className={`items menu-container ${window.innerWidth <= 768 ? '' : 'hidden'}`}>
-          <ul className="menu-container">
-            {menuItems.map((item, index) => (
-              <li key={index} onClick={() => toggleMoreOptions(item)}>
-                {item}
-                {selectedItem === item && <Icon icon="mdi:tick" className="tick-icon" />}
-              </li>
-            ))}
-          </ul>
+        <div className="items menu-container">
+        {windowWidth > 768 && (
+            <ul className="menu-container">
+              {menuItems.map((item, index) => (
+                <li key={index} onClick={() => toggleMoreOptions(item)}>
+                  {item}
+                  
+                </li>
+              ))}
+            </ul>
+        )}
         </div>
 
         <div className="dropdown-container" onClick={() => toggleMoreOptions(selectedItem)} ref={dropdownRef}>
-          <div className={`dropdown-main-text ${window.innerWidth <= 768 ? '' : 'visible'}`}>MORE</div>
-          <Icon icon="bi:chevron-down" className={`dropdown-icon ${window.innerWidth <= 768 ? 'visible' : ''}`} />
+          <div className="dropdown-main-text">MORE</div>
+          <Icon icon="bi:chevron-down" className="dropdown-icon" />
 
-          <ul className={`dropdown-list ${openMoreOptions ? 'visible' : ''}`}>
-            {moreOptions.map((option, index) => (
-              <li
-                key={index}
-                onClick={() => toggleMoreOptions(option)}
-                className={`dropdown-list-item ${selectedItem === option ? 'selected' : ''}`}
-              >
-                {option}
-                {selectedItem === option && <Icon icon="mdi:tick" className="tick-icon" />}
-              </li>
-            ))}
-          </ul>
+         <ul className={`dropdown-list ${openMoreOptions ? 'visible' : ''}`}>
+  {windowWidth <= 768
+    ? itemsToRender.map((item, index) => (
+        <li
+          key={index}
+          onClick={() => toggleMoreOptions(item)}
+          className={`dropdown-list-item ${selectedItem === item ? 'selected' : ''}`}
+        >
+          {item}
+          {selectedItem === item && <Icon icon="mdi:tick" className="tick-icon" />}
+        </li>
+      ))
+    : moreOptions.map((option, index) => (
+        <li
+          key={index}
+          onClick={() => toggleMoreOptions(option)}
+          className={`dropdown-list-item ${selectedItem === option ? 'selected' : ''}`}
+        >
+          {option}
+          {selectedItem === option && <Icon icon="mdi:tick" className="tick-icon" />}
+        </li>
+      ))}
+</ul>
+
         </div>
 
-        <div className="search">
+         <div className="search">
           <Icon icon="bi:search" className="search-icon" />
           <input type="text" placeholder="Search Something" className="search-box" />
-        </div>
+        </div> 
+
+
       </nav>
     </div>
   );
